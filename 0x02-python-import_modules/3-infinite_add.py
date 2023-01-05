8d33fe3624f4f3cafd2489dc54696913d7658d8a
harmonyordu <harmonyordu@gmail.com>
2023-01-05 14:13:21 -0800

    code corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+
 import sys
+
 if __name__ == "__main__":
-	arg_len = len(sys.argv)
-	res = 0
-	for i in range(1, arg_len):
-		res += int(sys.argv[i])
-	print(res)
+
+    arg_len = len(sys.argv)
+
+    res = 0
+
+    for i in range(1, arg_len):
+
+        res += int(sys.argv[i])
+
+    print(res)
